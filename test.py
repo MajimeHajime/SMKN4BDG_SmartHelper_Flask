b1526761299535f5e3436903fbc9715bba236e82
MajimeHajime <MajimeHajime@users.noreply.github.com>
2020-11-24 22:24:52 +0700

    dont open dis
     ini buat debug potongan kode yang harus di isolasi biar keliatan kelakuannya. (naughty code >,<)

---
+++ test.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for
+from PredictionModel import run_prediction
+from datetime import date, timedelta
+import plotly
+import plotly.express as px
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+import json
+import pickle
+
+
+app = Flask(__name__)
+
+today = date.today()
+todayish = today - timedelta(days=1)
+yesterday = today - timedelta(days=2)
+tomorrow = today + timedelta(days=1)
+outaded = bool(0)
+case = 0
+casePast = 0
+casePred = 0
+change = 0
+
+try:
+    lastRun = pickle.load(open("day.pickle", "rb"))
+except (OSError, IOError) as e:
+    lastRun = today
+    pickle.dump(lastRun, open("day.pickle", "wb"))
+lastRun = lastRun
+
+print(lastRun)
