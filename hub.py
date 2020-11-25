12c57c4947e724ec08f29eabe12cad60cb6316a1
MajimeHajime <MajimeHajime@users.noreply.github.com>
2020-11-25 23:58:27 +0700

    fix(?)

--- hub.py
+++ hub.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for
 from PredictionModel import run_prediction
 from datetime import date, timedelta
+from forms import RegistrationForm, LoginForm
 import plotly
 import plotly.express as px
 import plotly.graph_objs as go
@@ -11,6 +12,8 @@ import pickle
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '51c11bd0792105993631f9e22095b5f8'
+
 
 try:
     lastRun = pickle.load(open("day.pickle", "rb"))
@@ -32,7 +35,42 @@ def front():
     return render_template(
         'frontpage.html'
         )
+'''
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        if form.validate_on_submit():
+            if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+                flash('You have been logged in!', 'success')
+                return redirect(url_for('home'))
+            else:
+                flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template(
+        'login.html',
+        title='Login',
+        form=form
+        )
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash(
+            f'Account created for {form.username.data}!', 
+            'success'
+            )
+        return redirect(
+            url_for(
+                'home'
+                )
+            )
+    return render_template(
+        'register.html',
+        title='Register',
+        form=form
+        )
+'''
 @app.route('/stats')
 def stats():
     df = pd.read_csv('testfile.csv')
@@ -60,21 +98,18 @@ def create_plot():
     df = pd.read_csv('testfile.csv')
     fig = go.Figure(layout_title_text="Statistik Covid-19")
     fig.add_trace(go.Scatter(x=df["date"], y=df['predicted'], name="Past Prediction", line = dict(color='#2AA965', width=2)))
-    #px.line(df, x="date", y="predicted", color=px.Constant("Prediction"), line = dict(color='#2AA965', width=2, dash='dash'))
     fig.add_bar(x=df["date"], y=df["total"], name="Total Cases")
     fig.add_trace(go.Scatter(x=df["date"], y=df['y_fut'], name="Future Prediction", line = dict(color='#CC252C', width=2, dash='dot')))
     fig.update_traces(marker_color='#cbcbcb')
-    fig.layout.update(showlegend=False)
-    fig.layout.update(yaxis_title=" ")
-    fig.layout.update(xaxis_title=" ")
-    fig.update_layout(
+    fig.layout.update(
+        showlegend=False,
+        yaxis_title=" ",
+        xaxis_title=" ", 
         title_font_family="SF Pro Display Semibold",
         title_font_color="#1f1f1f",
         legend_title_font_color="green",
         font_family="SF Pro Display Semibold",
         font_color="#1f1f1f",
-    )
-    fig.update_layout(
         margin=dict(l=20, r=20, t=23, b=20),
         paper_bgcolor='rgba(0,0,0,0)',
         plot_bgcolor='#E1E1E1'
