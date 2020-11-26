fda93d64984b32b50e21fedcbd2e8614676bbc39
MajimeHajime <MajimeHajime@users.noreply.github.com>
2020-11-26 22:21:31 +0700

    login is usable

--- hub/__init__.py
+++ hub/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from hub.PredictionModel import run_prediction
-from datetime import date, timedelta, datetime
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '51c11bd0792105993631f9e22095b5f8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from hub import routes
