2fad6b5a5b8bb78ba15a6b366bff9a9c1542c1dc
MajimeHajime <MajimeHajime@users.noreply.github.com>
2020-11-27 02:15:12 +0700

    beeg

--- hub/routes.py
+++ hub/routes.py
@@ -1,11 +1,13 @@
 from flask import render_template, url_for, flash, redirect, request
 from hub import app, db, bcrypt
-from hub.forms import RegistrationForm, LoginForm
+from hub.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from hub.models import User, Post
-import pickle
+from PIL import Image
 from hub.variables import t_stat, y_stat, yoy_stat, p_stat, c_stat, c1_stat, news, news2, posts
 from flask_login import login_user, current_user, logout_user, login_required
-
+import pickle
+import secrets
+import os
 
 @app.route('/')
 def front():
@@ -23,7 +25,7 @@ def front():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -69,7 +71,36 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
